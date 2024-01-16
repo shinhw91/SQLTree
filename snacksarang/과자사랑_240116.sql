2882b961159d64dfe64fdec2b4e59446248f01f3
shinhw91 <shinhw48@gmail.com>
2024-01-16 17:42:39 +0900

    중간프로젝트 상품목록, 상세화면 템플릿 작업 및 데이터 추가

--- snacksarang/과자사랑_240116.sql
+++ snacksarang/과자사랑_240116.sql
@@ -3,7 +3,7 @@ drop table review;
 drop table detail;
 drop table orders;
 drop table cart;
-drop table goods;
+--drop table goods;
 drop table member;
 
 drop sequence detail_seq;
@@ -28,7 +28,16 @@ CREATE TABLE goods (
 	price	number		NULL,
 	category	varchar2(100)		NULL,
 	thumb_image	varchar2(100)		NULL,
-	detail_image	varchar2(100)		NULL
+	detail_image	varchar2(100)		NULL,
+	weight	number		NULL,
+	unit	varchar2(100)		NULL,
+	allergy	varchar2(100)		NULL,
+	calorie	number		NULL,
+	na	number		NULL,
+	carbo	number		NULL,
+	sugar	number		NULL,
+	sfat	number		NULL,
+	protein	number		NULL
 );
 
 CREATE TABLE orders (
@@ -46,6 +55,7 @@ CREATE TABLE orders (
 CREATE TABLE review (
 	review_code	number		NOT NULL,
 	goods_code	varchar2(100)		NOT NULL,
+	member_code	varchar2(100)		NULL,               
 	title	varchar2(500)		NULL,
 	content	clob		NULL,
 	stars	number		NULL,
@@ -131,18 +141,18 @@ insert into member values('M-00008', 'user8', '0000', '회원8', '010-8888-8888', 
 insert into member values('M-00009', 'user9', '0000', '회원9', '010-9999-9999', '대구시 중구 중앙대로', 'user9@naver.com');
 insert into member values('M-00010', 'user10', '0000', '회원10', '010-0000-0000', '대구시 중구 중앙대로', 'user10@naver.com');
 
-insert into goods values('G-00001', '상품1', '예담', 1000, '과자/쿠키', '새우깡.png', '새우깡.png');
-insert into goods values('G-00002', '상품2', '예담', 2000, '과자/쿠키', '새우깡.png', '새우깡.png');
-insert into goods values('G-00003', '상품3', '예담', 3000, '과자/쿠키', '새우깡.png', '새우깡.png');
-insert into goods values('G-00004', '상품4', '예담', 4000, '초콜릿/젤리', '새우깡.png', '새우깡.png');
-insert into goods values('G-00005', '상품5', '예담', 5000, '초콜릿/젤리', '새우깡.png', '새우깡.png');
-insert into goods values('G-00006', '상품6', '예담', 6000, '초콜릿/젤리', '새우깡.png', '새우깡.png');
-insert into goods values('G-00007', '상품7', '예담', 7000, '캔디/껌', '새우깡.png', '새우깡.png');
-insert into goods values('G-00008', '상품8', '예담', 8000, '캔디/껌', '새우깡.png', '새우깡.png');
-insert into goods values('G-00009', '상품9', '예담', 9000, '캔디/껌', '새우깡.png', '새우깡.png');
-insert into goods values('G-00010', '상품10', '예담', 10000, '음료/커피', '새우깡.png', '새우깡.png');
-insert into goods values('G-00011', '상품11', '예담', 11000, '음료/커피', '새우깡.png', '새우깡.png');
-insert into goods values('G-00012', '상품12', '예담', 12000, '음료/커피', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00001', '상품1', '예담', 1000, '과자/쿠키', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00002', '상품2', '예담', 2000, '과자/쿠키', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00003', '상품3', '예담', 3000, '과자/쿠키', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00004', '상품4', '예담', 4000, '초콜릿/젤리', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00005', '상품5', '예담', 5000, '초콜릿/젤리', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00006', '상품6', '예담', 6000, '초콜릿/젤리', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00007', '상품7', '예담', 7000, '캔디/껌', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00008', '상품8', '예담', 8000, '캔디/껌', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00009', '상품9', '예담', 9000, '캔디/껌', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00010', '상품10', '예담', 10000, '음료/커피', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00011', '상품11', '예담', 11000, '음료/커피', '새우깡.png', '새우깡.png');
+--insert into goods values('G-00012', '상품12', '예담', 12000, '음료/커피', '새우깡.png', '새우깡.png');
 
 create sequence cart_seq;
 
@@ -175,23 +185,25 @@ insert into detail values(detail_seq.nextval, 'O-00002', 'G-00010', 1, 10000);
 
 create sequence review_seq;
 
-insert into review values(review_seq.nextval, 'G-00001', '상품평1', '상품평입니다. 최소 30자 이상 작성해주세요.', 1, '2024-01-05');
-insert into review values(review_seq.nextval, 'G-00002', '상품평2', '상품평입니다. 최소 30자 이상 작성해주세요.', 1, '2024-01-05');
-insert into review values(review_seq.nextval, 'G-00003', '상품평3', '상품평입니다. 최소 30자 이상 작성해주세요.', 2, '2024-01-05');
-insert into review values(review_seq.nextval, 'G-00004', '상품평4', '상품평입니다. 최소 30자 이상 작성해주세요.', 2, '2024-01-05');
-insert into review values(review_seq.nextval, 'G-00005', '상품평5', '상품평입니다. 최소 30자 이상 작성해주세요.', 3, '2024-01-05');
-insert into review values(review_seq.nextval, 'G-00006', '상품평6', '상품평입니다. 최소 30자 이상 작성해주세요.', 3, '2024-05-12');
-insert into review values(review_seq.nextval, 'G-00007', '상품평7', '상품평입니다. 최소 30자 이상 작성해주세요.', 4, '2024-05-12');
-insert into review values(review_seq.nextval, 'G-00008', '상품평8', '상품평입니다. 최소 30자 이상 작성해주세요.', 4, '2024-05-12');
-insert into review values(review_seq.nextval, 'G-00009', '상품평9', '상품평입니다. 최소 30자 이상 작성해주세요.', 5, '2024-05-12');
-insert into review values(review_seq.nextval, 'G-00010', '상품평10', '상품평입니다. 최소 30자 이상 작성해주세요.', 5, '2024-05-12');
+insert into review values(review_seq.nextval, 'G-00001', 'M-00001', '상품평1', '상품평입니다. 최소 30자 이상 작성해주세요.', 1, '2024-01-05');
+insert into review values(review_seq.nextval, 'G-00002', 'M-00001', '상품평2', '상품평입니다. 최소 30자 이상 작성해주세요.', 1, '2024-01-05');
+insert into review values(review_seq.nextval, 'G-00003', 'M-00001', '상품평3', '상품평입니다. 최소 30자 이상 작성해주세요.', 2, '2024-01-05');
+insert into review values(review_seq.nextval, 'G-00004', 'M-00001', '상품평4', '상품평입니다. 최소 30자 이상 작성해주세요.', 2, '2024-01-05');
+insert into review values(review_seq.nextval, 'G-00005', 'M-00001', '상품평5', '상품평입니다. 최소 30자 이상 작성해주세요.', 3, '2024-01-05');
+insert into review values(review_seq.nextval, 'G-00006', 'M-00002', '상품평6', '상품평입니다. 최소 30자 이상 작성해주세요.', 3, '2024-05-12');
+insert into review values(review_seq.nextval, 'G-00007', 'M-00002', '상품평7', '상품평입니다. 최소 30자 이상 작성해주세요.', 4, '2024-05-12');
+insert into review values(review_seq.nextval, 'G-00008', 'M-00002', '상품평8', '상품평입니다. 최소 30자 이상 작성해주세요.', 4, '2024-05-12');
+insert into review values(review_seq.nextval, 'G-00009', 'M-00002', '상품평9', '상품평입니다. 최소 30자 이상 작성해주세요.', 5, '2024-05-12');
+insert into review values(review_seq.nextval, 'G-00010', 'M-00002', '상품평10', '상품평입니다. 최소 30자 이상 작성해주세요.', 5, '2024-05-12');
 
 -- 테이블 조회
-select * from member;
-select * from goods;
-select * from cart;
-select * from detail;
-select * from orders;
-select * from review;
+select * from member order by 1;
+select * from goods order by 1;
+select * from cart order by 1;
+select * from detail order by 1;
+select * from orders order by 1;
+select * from review order by 1;
 
 commit;
+
+select * from goods where goods_name like '%상%';리', '새우깡.png', '새우깡.png');
